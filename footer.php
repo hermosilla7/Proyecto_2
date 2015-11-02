08e4b5057fb33c783c46e733282eabb63af7d132
unknown <8706.joan23@415-PORTC1197NC.joan23.local>
2015-11-02 13:38:02 +0100

    header&footer v1

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<footer>
+	<div>
+		Copyright 2003-2015 _ Desarrollador 
+	</div>
+	<div>
+		Club_Estudio
+	</div>
+	<div>
+
+	</div>
+</footer>
---
+++ header.php
@@ -0,0 +1,19 @@
+
+	<header>
+		<div class="">
+			
+		</div>
+		<div class="">
+			<img src="img/imgclubestudio.jpg" alt="Logo Club estudio" style="width:25px;height:25px">
+		</div>
+	</header>
+	<nav class="navbar navbar-default" style="margin-top:-20px">
+		<div class="container-fluid">
+		    <div class="navbar-header">
+		    	<a class="navbar-brand" href="">Formulari millores NET</a>
+		    </div>
+		    <div class="navbar-text navbar-right">
+		    	<img src="images/favicon.png" alt="Logo Jesuïtes" style="width:25px;height:25px" />&nbsp;Jesuïtes Educació
+		    </div>
+		</div>
+	</nav>
