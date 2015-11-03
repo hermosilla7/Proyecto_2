1db683793e85c8ef8f67c00e5a4519b8898a2c59
unknown <8706.joan23@415-PORTC1197NC.joan23.local>
2015-11-03 12:38:18 +0100

    header actualizado

--- busqueda.php
+++ busqueda.php
@@ -8,6 +8,7 @@ $result_recurso = mysqli_query($con, $consulta_recurso);
 $result_categoria = mysqli_query($con, $consulta_categoria);
 
 ?>
+	<p class="divEric">
 		<form action="resultados.php" method="GET">
 			<input id="checkbox" type="checkbox" name="estado_recurso[]" value="0">Disponible<br/><br>
 			<input id="checkbox" type="checkbox" name="estado_recurso[]" value="1">Ocupado<br/><br>
@@ -26,7 +27,7 @@ $result_categoria = mysqli_query($con, $consulta_categoria);
 			<input id="boton" type="reset" value="Cancelar">
 			<input id="boton" type="button" onclick="alert('Rellena los campos necesarios')" value="?">
 
-
+	</p>
 
 		</form>
 		<?php  include "footer.php";
