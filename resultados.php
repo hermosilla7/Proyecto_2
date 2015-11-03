8dc13d686c80020842c657d0cb5ed944099b7f4f
PerezRaul <7400.joan23@fje.edu>
2015-11-03 21:26:13 +0100

    login finalizado, ventana modal para reservar y liberar, algo de diseño

--- resultados.php
+++ resultados.php
@@ -1,67 +1,116 @@
 <?php
-include_once 'header.php';
-include 'conexion.php';
+	include 'conexion.php';
+	include_once 'header.php';
 
-//como la sentencia SIEMPRE va a buscar todos los registros de la tabla producto, pongo en la variable $sql esa parte de la sentencia que SI o SI, va a contener
-$sql = "SELECT * FROM recurso WHERE ";
 
-//VERSION BETA
-//controlar checkbox
-if(!isset($_REQUEST['estado_recurso'])){
-	echo "Selecciona disponible u ocupado";
-} else {
-	$count = 0;
-	foreach ($_REQUEST['estado_recurso'] as $opcionEstado[]) {
-	$count+=1;			
-	}
+	//como la sentencia SIEMPRE va a buscar todos los registros de la tabla producto, pongo en la variable $sql esa parte de la sentencia que SI o SI, va a contener
+	$sql = "SELECT * FROM recurso WHERE ";
 
-	if ($count==0) {
-		$sql = "";
-	}
-	if ($count>0) {
-		$sql.= " (estado=$opcionEstado[0]";
-		if ($count>1){
-			$sql.= " OR estado=$opcionEstado[1]";
+	//VERSION BETA
+	//controlar checkbox
+	if(!isset($_REQUEST['estado_recurso'])){
+		echo "Selecciona disponible u ocupado";
+	} else {
+		$count = 0;
+		foreach ($_REQUEST['estado_recurso'] as $opcionEstado[]) {
+		$count+=1;			
 		}
-		$sql.=")";
-	}
 
-	//DATOS MUNICIPIO
-	if(($_REQUEST['categoria'] == '')){
-		// echo "No se muestra municipio";
-	}
-	else {
-	$categoria=$_REQUEST['categoria'];
-	$sql .= " AND categoria = $categoria";
-	}
-
-
-	$datos = mysqli_query($con, $sql);
-	//extraemos los productos uno a uno en la variable $anuncio que es un array
-	while($recurso = mysqli_fetch_array($datos)){
-		echo "<b>Nombre:</b>";
-		echo utf8_encode($recurso['nombre']);
-		echo "<br/>";
-		echo "<b>Contenido:</b> ";
-		echo utf8_encode($recurso['descr']);
-		echo "<br/>";
-
-		$fichero="img/$recurso[img]";
-		if(file_exists($fichero)&&(($recurso['img']) != '')){
-			echo "<img src='$fichero' width='80' heigth='80' ><br/><br/><br/>";
+		if ($count==0) {
+			$sql = "";
 		}
-		else{
-			echo "<img src ='img/no_disponible.jpg'/>";
+		if ($count>0) {
+			$sql.= " (estado=$opcionEstado[0]";
+			if ($count>1){
+				$sql.= " OR estado=$opcionEstado[1]";
+			}
+			$sql.=")";
 		}
-		?>
-		<a href="reservar.php?id_recurso=<?php echo $recurso['id_recurso']; ?>">Reservar</a>
-		<?php
-		echo "<br/><br>";
-		
+
+		//DATOS MUNICIPIO
+		if(($_REQUEST['categoria'] == '')){
+			// echo "No se muestra municipio";
+		}
+		else {
+		$categoria=$_REQUEST['categoria'];
+		$sql .= " AND categoria = $categoria";
+		}
+
+
+		$datos = mysqli_query($con, $sql);
+		//extraemos los productos uno a uno en la variable $anuncio que es un array
+		while($recurso = mysqli_fetch_array($datos)){
+			echo "<b>Nombre:</b>";
+			echo utf8_encode($recurso['nombre']);
+			echo "<br/>";
+			echo "<b>Contenido:</b> ";
+			echo utf8_encode($recurso['descr']);
+			echo "<br/>";
+
+			$fichero="img/$recurso[img]";
+			if(file_exists($fichero)&&(($recurso['img']) != '')){
+				echo "<img src='$fichero' width='80' heigth='80' ><br/><br/><br/>";
+			}
+			else{
+				echo "<img src ='img/no_disponible.jpg'/>";
+			}
+			?>
+			<a href="reservar.php?id_recurso=<?php echo $recurso['id_recurso']; ?>">Reservar</a>
+
+
+			<button type="button" class="btn btn-primary" id="botonOpciones" data-toggle="modal" data-target="#modalOpciones">Opciones</button>
+			<div class="modal fade" id="modalOpciones">
+	            <div class="modal-dialog">
+	                <div class="modal-content">
+	                    <div class="modal-header">
+	                        <h4 class="modal-title">Reserva del recurso</h4>
+	                    </div>
+	                    
+	                    <div class="modal-body">
+	                        <div class="form-group">
+	                            <label class="control-label col-sm-3" for="user">Nombre del recurso:</label>
+	                            <div class="col-sm-7">          
+	                                <input type="text" class="form-control" id="nombre" name="nombre" readonly />
+	                            </div>
+	                        </div><br /><br />
+	                        <div class="form-group">
+	                            <label class="control-label col-sm-3" for="passwd">Tipo de recurso:</label>
+	                            <div class="col-sm-7">          
+	                                <input type="text" class="form-control" id="tipo" name="tipo" value="hola" readonly />
+	                            </div>
+	                        </div><br /><br />
+	                    </div>
+
+	                    <div class="modal-footer">
+	                        <button type="submit" class="btn btn-success" id="reservar">Reservar</button>
+	                        <button type="submit" class="btn btn-primary" id="liberar">Liberar</button>
+	                        <button type="submit" class="btn btn-danger" id="cancelar" onclick="cerrar()">Salir</button>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+
+	        <script>
+	        	$('#modalOpciones').modal({
+	        		show: false,
+	        		backdrop: 'static',
+	        		keyboard: false,
+	        	})
+
+	        	function cerrar(){
+	        		$('#modalOpciones').modal('hide');
+	        	}
+	        </script>
+
+			<?php
+			echo "<br/><br>";
+			
+		}
 	}
-}
-//cerramos la conexión con la base de datos
-mysqli_close($con);
+	//cerramos la conexión con la base de datos
+	mysqli_close($con);
+
+
+	include "footer.php";
+
 ?>
-
-<?php include "footer.php";
--- user.php
+++ user.php
@@ -29,7 +29,7 @@
 		
 		<!-- Generic page styles -->
 		<link rel="stylesheet" href="css/style.css">
-		<link rel="icon" type="image/png" href="images/favicon.png" />
+		<link rel="icon" type="image/png" href="img/favicon.png" />
 	</head>
 	<body>
 		<?php
@@ -41,25 +41,54 @@
 			$result_recurso = mysqli_query($con, $consulta_recurso);
 			$result_categoria = mysqli_query($con, $consulta_categoria);
 
-			?>
-			<form action="resultados.php" method="GET">
-				<input id="checkbox" type="checkbox" name="estado_recurso[]" value="0">Disponible<br/><br>
-				<input id="checkbox" type="checkbox" name="estado_recurso[]" value="1">Ocupado<br/><br>
 
-			   	<!-- Categoria -->
-				<select id="categoria" name="categoria">
-						<option value="">Seleccionar categoría</option>
-						<?php
-						while($fila=mysqli_fetch_array($result_categoria)){
-							echo utf8_encode("<option value=\"$fila[id]\">$fila[nombre]</option>");
-						}
-			        	?>
-			    </select><br/><br>
+			//creamos la sesion
+			session_start();
 
-					<input id="boton" type="submit" value="Enviar">
-					<input id="boton" type="reset" value="Cancelar">
-					<input id="boton" type="button" onclick="alert('Rellena los campos necesarios')" value="?">
-			</form>
+			$nomUsuari = $_SESSION['nom'];
+
+			//validamos si se ha hecho o no el inicio de sesion correctamente
+			//si no se ha hecho la sesion nos regresará a index.html
+			if(!isset($_SESSION['nom'])) 
+			{
+			  header('Location: index.html'); 
+			  exit();
+			}
+		?>
+		<form action="resultados.php" method="GET">
+			<div class="checkbox">
+				<label class="checkbox-inline">
+					<input id="checkbox" type="checkbox" name="estado_recurso[]" value="0">Disponible
+				</label>
+				<label class="checkbox-inline">
+					<input id="checkbox" type="checkbox" name="estado_recurso[]" value="1">Ocupado
+				</label>
+			</div>
+			
+		   	<!-- Categoria -->
+			<select class="btn btn-default" id="categoria" name="categoria">
+					<option value="">Seleccionar categoría</option>
+					<?php
+					while($fila=mysqli_fetch_array($result_categoria)){
+						echo utf8_encode("<option value=\"$fila[id]\">$fila[nombre]</option>");
+					}
+		        	?>
+		    </select><br/><br>
+
+				<input class="btn btn-default" id="botonEnviar" type="submit" value="Enviar">
+				<input class="btn btn-danger" id="botonCancelar" type="reset" value="Cancelar">
+				<input class="btn btn-success" id="botonAyuda" type="button" value="?">
+		</form>
+
+		<script type="text/javascript">
+			$('#botonAyuda').popover({
+				title: 'Ayuda',
+				content: 'Rellena los campos necesarios',
+				placement: 'right'
+			});
+
+			
+		</script>
 		
 
 
@@ -69,11 +98,11 @@
 		        <div class="modal-content">
 		            <div class="modal-header">
 		                <button type="button" class="close" id="close" data-dismiss="modal" aria-hidden="true">×</button>
-		                <h4 class="modal-title">Millores NET Jesuïtes Educació </h4>
+		                <h4 class="modal-title">Incidencias Club Estudio</h4>
 		            </div>
 		            <div class="modal-body">
-							<p>La teva millora ha estat registrada.En breu sabràs l'estat de la millora. </p>
-							<p><b>Moltes gràcies!</b></p>
+							<p>Tu incidencia ha sido registrada. En breve sabrás el estado de la incidencia.</p>
+							<p><b>Muchas grácias!</b></p>
 		            </div>
 		        </div>
 		    </div>
@@ -82,270 +111,42 @@
 		
 		<div class="container" style="margin-top:10px">
 			<!-- FORMULARIO PRINCIPAL DE DONDE OBTENDRÁ LOS DATOS LA TABLA -->
-			<!-- <form id="frmMillora" name="frmMillora" role="form" action="updateReg.php">
+			<form id="frmIncidencia" name="frmIncidencia" role="form" action="updateReg.php">
 				<div class="form-group">
 					<?php
-						echo "<input name='usuari' id='usuari' type='text' class='form-control' data-bv-field='usuari' value='$nomUsuari' style='display:none' />"
+						echo "<input name='usuari' id='usuari' type='text' class='form-control' value='$nomUsuari' style='display:none' />"
 	            	?>
-	            </div> -->
+	            </div>
 
 	            <!-- PARTE DONDE SE ENCUENTRA EL TITULO, AREA Y DESCRIPCION DE LA MEJORA -->
-				<!-- <div class="row" style="width:100%;margin-top:20px">
-				    <h1 style="margin-left:15px">Possibles millores</h1>
+				<div class="row" style="width:100%;margin-top:20px">
+				    <h1 style="margin-left:15px">Posibles incidencias</h1>
 					<div class="col-md-20" style="margin-left:20px">
 			            <div class="panel panel-default">
 			                <div class="panel-body"> 
 			                	<div class="form-inline form-group"> 
-				                	<input name="titol" id="titol" type="text" class="form-control" placeholder="Títol de la millora" data-bv-field='titol' maxlength="25" size="68" />
-				                	<select class="btn btn-default" id="area" name="area" data-bv-field='area'>
-				                		<option value="Escollir area">Escollir àrea</option>
-				                		<option value="Arxius">Arxius</option>
-				                		<option value="Calendari">Calendari</option>
-				                		<option value="Centre Notificacions">Centre Notificacions</option>
-				                		<option value="Configuració">Configuració</option>
-				                		<option value="Disseny">Disseny</option>
-				                		<option value="Inici">Inici</option>
-				                		<option value="Llista">Llista</option>
-				                		<option value="Lloc Web">Lloc Web</option>
-									  	<option value="NET General">NET General</option>
+				                	<input name="titulo" id="titulo" type="text" class="form-control" placeholder="Titulo de la incidencia" maxlength="50" size="68" />
+				                	<select class="btn btn-default" id="recurso" name="recurso">
+				                		<option value="Escoger recurso">Escoger recurso</option>
+				                		<option value="Aula">Aula</option>
+				                		<option value="Portatil">Portatil</option>
 									</select>
 			                	</div>
 				                <div class="form-group">
-				                	<textarea class="form-control counted" name="millora" id="millora" placeholder="Descripció" rows="5" style="margin-bottom:10px;width:100%" data-bv-field='millora'></textarea>
+				                	<textarea class="form-control counted" name="contenido" id="contenido" placeholder="Descripción" rows="5" style="margin-bottom:10px;width:100%"></textarea>
 			                	</div>
 			                	<h6 class="pull-left" id="counter">500 caràcters encara</h6>
 		            			<button class="btn btn-flat btn-primary pull-right" id="enviar" name="enviar" type="submit">Envia</button>
 			                </div>
 			            </div>
 		            </div>             
-			    </div> -->
-
-			    <!-- PARTES OCULTAS PARA QUE LEA UN VALOR EN LA BASE DE DATOS -->
-			    <!-- <div class="form-group" style="display:none;">
-			        <label >Puntuació</label>
-			        <div class="col-xs-10">
-			            <input id="puntuacio" name="puntuacio" type="number" />
-			        </div>
 			    </div>
-			    <div class="form-group" style="display:none;">
-			        <label class="col-xs-2 control-label">Prioritat</label>
-			        <div class="col-xs-10">
-			            <select class="btn btn-default" name="prioritat" id="prioritat">
-						    <option value="1">1</option>
-						    <option value="2">2</option>
-						    <option value="3">3</option>
-						    <option value="4">4</option>
-						    <option value="5">5</option>
-						</select>
-			        </div>
-			    </div>	
-			    <div class="form-group" style="display:none;">
-			        <label class="col-xs-2 control-label">Estat</label>
-			        <div class="col-xs-10">
-			            <select class="btn btn-default" name="estat" id="estat">
-						    <option value="Pendent">Pendent</option>
-						    <option value="En procés">En procés</option>
-						    <option value="Descartada">Descartada</option>
-						    <option value="Finalitzada">Finalitzada</option>
-						</select>
-			        </div>
-			    </div>
-			</form> -->
-
-
-			<!-- FORMULARIO ESTRUCTURADO PARA QUE APAREZCA EN LA VENTANA MODAL -->
-			<!-- <form id="frmModal" class="form-horizontal" style="display: none;" role="form" action="updateModal_admin.php">
-				<div class="form-group" style="display: none;">
-			        <label class="col-xs-2 control-label">ID_Millora</label>
-			        <div class="col-xs-10">
-			            <input type="text" class="form-control" id="id_millora" name="id_millora" readonly="readonly" />
-			        </div>
-			    </div>
-			    <div class="form-group">
-			        <label class="col-xs-2 control-label">Autor</label>
-			        <div class="col-xs-10">
-			            <input type="text" class="form-control" id="usuari2" name="usuari2" readonly="readonly" />
-			        </div>
-			    </div>
-			    <div class="form-group">
-			        <label class="col-xs-2 control-label">Títol</label>
-			        <div class="col-xs-10">
-			            <input type="text" class="form-control" id="titol2" name="titol2" readonly="readonly" />
-			        </div>
-			    </div>	
-			    <div class="form-group">
-			        <label class="col-xs-2 control-label">Descripció</label>
-			        <div class="col-xs-10">
-			            <textarea name="millora2" id="millora2" rows="6"  class="form-control noresize" readonly="readonly"></textarea>
-			        </div>
-			    </div>
-			    <div class="form-group">
-			        <label class="col-xs-2 control-label">Àrea</label>
-			        <div class="col-xs-4">
-		        		<select class="btn btn-default" id="area2" name="area2" data-bv-field='area'>
-	                		<option value="Escollir area">Escollir àrea</option>
-	                		<option value="Arxius">Arxius</option>
-	                		<option value="Calendari">Calendari</option>
-	                		<option value="Centre Notificacions">Centre Notificacions</option>
-	                		<option value="Configuració">Configuració</option>
-	                		<option value="Disseny">Disseny</option>
-	                		<option value="Inici">Inici</option>
-	                		<option value="Llista">Llista</option>
-	                		<option value="Lloc web">Lloc Web</option>
-						  	<option value="NET General">NET General</option>
-						</select>
-		        	</div>
-			    </div>
-			    <div class="form-group">
-			        <label class="col-xs-2 control-label">Prioritat</label>
-			        <div class="col-xs-10">
-			            <select class="btn btn-default" name="prioritat2" id="prioritat2">
-						    <option value="1">1</option>
-						    <option value="2">2</option>
-						    <option value="3">3</option>
-						    <option value="4">4</option>
-						    <option value="5">5</option>
-						</select>
-			        </div>
-			    </div>	
-			    <div class="form-group">
-			        <label class="col-xs-2 control-label">Estat</label>
-			        <div class="col-xs-10">
-			            <select class="btn btn-default" name="estat2" id="estat2">
-						    <option value="Pendent">Pendent</option>
-						    <option value="En procés">En procés</option>
-						    <option value="Descartada">Descartada</option>
-						    <option value="Finalitzada">Finalitzada</option>
-						</select>
-			        </div>
-			    </div>
-			</form> -->
-
-			<!-- TITULO Y PRINCIPIO DE LA TABLA BOOTSTRAP -->
-    		<!-- <h1>Millores proposades pels usuaris</h1> -->
-			<table id="tablaJS" data-toolbar="#custom-toolbar"></table>
+			</form>
 		</div>
-
 
 		<!-- SCRIPT PARA CONSTRUIR LA TABLA BOOTSTRAP Y EL VALIDADOR DEL FORUMLARIO -->
 		<script>
 			$(function () {
-			     $('#tablaJS').bootstrapTable({
-			         method: 'get',
-			         url: 'loadReg.php',
-					queryParams: function (p) {
-						return { titol: $('#titol').val() };
-					},
-			         cache: false,
-			         height: 500,
-			         striped: true,
-			         pagination: true,
-			         pageSize: 100,
-			         pageList: [10, 25, 50, 100, 200],
-			         search: true,
-			         showColumns: true,
-			         showRefresh: true,
-					 showToggle: false,
-			         minimumCountColumns: 2,
-			         clickToSelect: false,
-			         columns: [{
-			             field: 'nombre',
-			             title: 'Nombre del recurso',
-			             align: 'left',
-			             valign: 'middle',
-			             sortable: true
-			         }, {
-			             field: 'contenido',
-			             title: 'Descripción',
-			             align: 'center',
-			             valign: 'middle',
-			             sortable: true
-			         }, {
-			             field: 'imagen',
-			             title: 'Imágen',
-			             align: 'center',
-			             valign: 'middle',
-			             sortable: true
-			         }/*, {
-			         	 field: 'prioritat',
-			         	 title: 'Prioritat',
-			         	 align: 'center',
-			         	 valign: 'middle',
-			         	 sortable: true
-			         }, {
-			             field: 'estat',
-			             title: 'Estat',
-			             align: 'center',
-			             valign: 'middle',
-			             sortable: true
-			         }, {
-			             field: 'mitjana',
-			             title: 'Puntuació',
-			             align: 'center',
-			             valign: 'middle',
-						 sortable: true,
-						 formatter: starFormatter
-			         }*/]
-		       	 }).on('click-row.bs.table', function (e, row, $element) {
-				         //alert('Event: click-row.bs.table, data: ' + JSON.stringify(row));
-						
-						$('#frmModal')
-						 .find('[name="id_millora"]').val(row.id_millora).end()
-						 .find('[name="usuari2"]').val(row.autor).end()
-						 .find('[name="titol2"]').val(row.titol).end()
-						 .find('[name="millora2"]').val(row.millora).end()
-						 .find('[name="area2"]').val(row.area).end()
-						 .find('[name="prioritat2"]').val(row.prioritat).end()
-						 .find('[name="estat2"]').val(row.estat).end()
-						 .find('[name="puntuacio2"]').val(row.puntuacio).end();
-
-						bootbox
-							.dialog({
-								title: 'Visualització de la millora',
-								message: $('#frmModal'),
-								show: false, // We will show it manually later
-								buttons: {
-									success: {
-										label: "Guardar",
-										className: "btn-flat btn-success",
-										callback: function (){
-											$.post( $("#frmModal").attr("action"), 
-												$("#frmModal").serializeArray(),function(data){ 
-													$('#tablaJS').bootstrapTable('refresh');
-												}
-											);
-										}
-									},
-									main: {
-										label: "Tancar",
-										className: "btn-flat btn-default",
-										callback: function() {
-														
-										}
-									}
-								}
-							})
-							.on('shown.bs.modal', function() {
-								$('#frmModal')
-				                 .show()                             // Show the login form
-								})
-							.on('hide.bs.modal', function(e) {
-								// Bootbox will remove the modal 
-								// after hiding the modal
-								// Therefor, we need to backup the form
-								$('#frmModal').hide().appendTo('body');
-							})
-							.modal('show');				
-						
-						
-					});
-					
-					/* SOLUCION PARA REDIMENSIONAR AUTOMATICAMENTE LOS CAMPOS DE LA TABLA */
-
-					$(window).resize(function () {
-				        $('#tablaJS').bootstrapTable('resetView');
-				    });
-
 				/* VALIDACION DE LOS CAMPOS DEL FORMULARIO, SI ESTAN RELLENOS O NO */
 				$('#frmMillora').bootstrapValidator({
 			        framework: 'bootstrap',
@@ -416,7 +217,7 @@
 					settings = $.extend({
 						container: "<span></span>",
 						classname: "charcounter",
-						format: "(%1 caràcters restants)",
+						format: "(%1 caracteres restantes)",
 						pulse: true,
 						delay: 0
 					}, settings);
