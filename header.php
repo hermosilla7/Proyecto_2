6b5fc173d1681b893a20742b05d06a8b971e7bf6
PerezRaul <7400.joan23@fje.edu>
2015-11-03 10:47:05 +0100

    favicon fail

--- header.php
+++ header.php
@@ -33,22 +33,18 @@
     
     <!-- Generic page styles -->
     <link rel="stylesheet" href="css/style.css">
-    <link rel="icon" type="image/png" href="images/favicon.png" />
-  <link rel="stylesheet" type="text/css" href="css/global.css">
-  <link rel="icon" type="image/png" href="img/logo1.png" />
+    <link rel="stylesheet" type="text/css" href="css/global.css">
+    <link rel="icon" type="image/png" href="img/favicon.png" />
 </head>
 <body>
   <header>
-  <!--  <div class="">
-        <img src="img/logo.png" alt="Logo Club estudio" class="headerImage">
-    </div> -->
   <nav class="navigation">
         <ul>
           <li><a href="" class="navList">Inicio</a></li>
           <li><a href="" class="navList">Nav2</a></li>
           <li><a href="" class="navList">Nav3</a></li>
           <li><a href="" class="navList">Nav4</a></li>
-          <li><a href="" onClick="logout()" class="navLogout">Salir</a></li>
+          <li><a href="logout.php" class="navLogout">Salir</a></li>
 
         </ul>
         
