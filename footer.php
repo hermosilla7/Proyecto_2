d0790312c68dfa655399aadce1d79fd938421d0b
hermosilla7 <10000584.joan23@fje.edu>
2015-11-03 08:12:29 +0100

    header and footer

--- footer.php
+++ footer.php
@@ -1,12 +1,21 @@
 
-<footer>
-	<div>
-		Copyright 2003-2015 _ Desarrollador 
-	</div>
-	<div>
-		Club_Estudio
-	</div>
-	<div>
+	<footer class="footer-center">
 
-	</div>
-</footer>
+
+		<p class="footer-slogan">Academia de estudios dedicada a la educación</p>
+
+
+			<p class="footer-links">
+				
+				<a href="#">Inicio</a>
+				·
+				<a href="#">Contacto</a>
+
+			</p>
+
+			<div>
+				<div class="footer-name">Club estudio &copy; 2015</div>
+
+
+			</div>
+	</footer>
