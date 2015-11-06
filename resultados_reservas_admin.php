e5937d8e85d1f7de5f3a2bd67aab4f83bd81d572
hermosilla7 <10000584.joan23@fje.edu>
2015-11-06 11:48:07 +0100

    mostrar imagenes reservas 2

--- resultados_reservas_admin.php
+++ resultados_reservas_admin.php
@@ -35,6 +35,17 @@ function mostrarReservasAdmin(){
 				echo "<b>Fecha fin:</b> ";
 				echo utf8_encode($reserva['datefi']);
 				echo "<br><br/>";
+
+				$fichero="img/$recurso[img]";
+				if(file_exists($fichero)&&(($recurso['img']) != '')){
+					echo "<div class='contimg'><img src='$fichero' width='250' heigth='250' ></div>";
+				}
+				else{
+					echo "<div class='contimg'><img src ='img/no_disponible.jpg'width='250' heigth='250'/></div>";
+				}
+			
+			echo"</div>";
+			echo "<br/><br>";
 			}
 		}
 		else{
@@ -60,6 +71,16 @@ function mostrarReservasAdmin(){
 					echo "<b>Fecha fin:</b> ";
 					echo utf8_encode($reserva['datefi']);
 					echo "<br><br/>";
+					$fichero="img/$recurso[img]";
+				if(file_exists($fichero)&&(($recurso['img']) != '')){
+					echo "<div class='contimg'><img src='$fichero' width='250' heigth='250' ></div>";
+				}
+				else{
+					echo "<div class='contimg'><img src ='img/no_disponible.jpg'width='250' heigth='250'/></div>";
+				}
+			
+			echo"</div>";
+			echo "<br/><br>";
 	}
 }
 
